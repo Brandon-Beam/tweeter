fab57959a36277c81b575c35a84e80b5493b1f0c
Brandon <bbeam1@gmail.com>
2022-12-08 20:34:26 -0700

    did linting, doesnt seem to like jquery names like $ and document

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,33 +28,33 @@ const createTweetElement = (data) => {
     </p>
   </footer>
 </article>
-`
-  return $tweet
-}
+`;
+  return $tweet;
+};
 
 const renderTweets = function (tweets) {
   for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet)
+    const $tweet = createTweetElement(tweet);
     $(document).ready(function () {
       $('#tweets-container').prepend($tweet);
-    })
+    });
   }
-}
+};
 
 
 $(document).ready(function () {
   $.ajax('/tweets', { method: 'GET' })
     .then(function (data) {
       renderTweets(data);
-    })
+    });
   $("#new-tweet").submit(function (event) {
     const cereal = $(this).serialize();
-    $(this).parents(".container").find("#error-text").hide(1000)
+    $(this).parents(".container").find("#error-text").hide(1000);
     if ($(this).find("textarea").val() === "") {
-      $(this).parents(".container").find("#error-text").text("not enough text").show(1000)
+      $(this).parents(".container").find("#error-text").text("not enough text").show(1000);
     }
     if ($(this).find("textarea").val().length > 140) {
-      $(this).parents(".container").find("#error-text").show(1000)
+      $(this).parents(".container").find("#error-text").show(1000);
 
     } else {
       $.post("/tweets", cereal)
@@ -62,18 +62,19 @@ $(document).ready(function () {
           $.ajax('/tweets', { method: 'GET' })
             .then(function (data) {
               renderTweets(data);
-            })
-        })
-      $(this).find("textarea").val('')
-    };
+            });
+        });
+      $(this).find("textarea").val('');
+      $(this).find(".counter").val('140');
+    }
     event.preventDefault();
   });
-})
+});
 
 $(document).ready(function () {
   $(".focus").click(function () {
     $(this).parents("body").find("textarea").focus();
-  })
-})
+  });
+});
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
   $("textarea").keyup(function () {
-    $(this).parent("form").find(".counter").text(140 - ($(this).val().length))
+    $(this).parent("form").find(".counter").text(140 - ($(this).val().length));
     if ($(this).val().length > 140) {
-      $(this).parent("form").find(".counter").addClass("invalid")
+      $(this).parent("form").find(".counter").addClass("invalid");
     }
-
-
-  })
+  });
 });
-//140 - ($(this).val().length)
